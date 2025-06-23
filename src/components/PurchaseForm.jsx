26c17a155c8eb32c37718c099502857dfe78aad0
Rishabh Didwania <rishabhd@maqsoftware.com>
2025-06-23 09:00:02 +0530

    Testing

--- src/components/PurchaseForm.jsx
+++ src/components/PurchaseForm.jsx
@@ -22,7 +22,7 @@ export default function PurchaseForm() {
   const [successMsg, setSuccessMsg] = useState('');
 
   const predefinedAmounts = [10000, 20000, 50000, 75000, 100000];
-  const MIN_AMOUNT = 10000;
+  const MIN_AMOUNT = 2;
 
   // Load Razorpay script
   useEffect(() => {
