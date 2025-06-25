0c6a26d98e1d4cf54223537c60e8a83183545d1a
Rishabh Didwania <rishabhd@maqsoftware.com>
2025-06-25 10:00:52 +0530

    Updated ping

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,6 @@
 // src/pages/Home.jsx
+import { useEffect } from 'react';
+import axios from 'axios';
 import Navbar from '../components/NavbarLanding';
 import Hero from '../components/LandingHero';
 import PurchaseForm from '../components/PurchaseForm';
@@ -7,6 +9,18 @@ import Features from '../components/FeatureSection';
 import Footer from '../components/Footer';
 
 const LandingPage = () => {
+
+  useEffect(() => {
+    // Ping the backend when the component mounts
+    axios.get('https://byoc-backend.onrender.com/', { timeout: 3000 }) // Replace with your backend health endpoint
+      .then(res => {
+        console.log("Backend wakeup ping success:", res.status);
+      })
+      .catch(err => {
+        console.error("Backend wakeup ping failed:", err.message);
+      });
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-900 text-white relative">
          
