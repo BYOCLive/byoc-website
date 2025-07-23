2bc5a2101ae27ea6202a7208d242e2a862ecd0a9
Rishabh Didwania <99093735+RDRishabh@users.noreply.github.com>
2025-07-23 06:59:17 +0530

    Updated value

--- src/components/PurchaseForm.jsx
+++ src/components/PurchaseForm.jsx
@@ -23,7 +23,7 @@ export default function PurchaseForm() {
 
   // const predefinedAmounts = [10000, 20000, 50000, 75000, 100000];
   const predefinedAmounts = [500, 1000, 2000, 5000, 10000];
-  const MIN_AMOUNT = 200;
+  const MIN_AMOUNT = 500;
 
   // Load Razorpay script
   useEffect(() => {
