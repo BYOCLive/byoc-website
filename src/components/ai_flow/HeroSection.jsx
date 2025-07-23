bb271a59760dca51ef868ec450d9863e0d33571f
Rishabh Didwania <99093735+RDRishabh@users.noreply.github.com>
2025-07-23 07:04:46 +0530

    Updated a text

--- src/components/ai_flow/HeroSection.jsx
+++ src/components/ai_flow/HeroSection.jsx
@@ -12,7 +12,7 @@ export default function HeroSection() {
       <div className="max-w-4xl mt-24 w-full">
         <h1 className="text-5xl md:text-5xl font-bold mb-6">
           <span className="bg-gradient-to-r from-teal-400 via-blue-500 to-purple-600 bg-clip-text text-transparent">
-            50+ Premium AI Tools for Just Rs. 200
+            50+ Premium AI Tools for Just ₹200
           </span>
         </h1>
         <h2 className="text-2xl md:text-3xl font-bold text-white mb-6">
